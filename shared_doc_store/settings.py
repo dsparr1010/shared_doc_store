fd7be7a6b53fdd37e6bb48941b60c0480d83e2b7
Debra Sparr <dsparr@restaurant.com>
2021-10-14 15:31:06 -0500

    comment out old whitenoise storage config

--- shared_doc_store/settings.py
+++ shared_doc_store/settings.py
@@ -22,7 +22,6 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(f'BASEDIR:{BASE_DIR}')
 
 
 # Quick-start development settings - unsuitable for production
@@ -158,7 +157,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
